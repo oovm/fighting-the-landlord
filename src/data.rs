ec9ad6a1d6cf9f618427db564c834ed1aedc5c75
Aster <galaster@foxmail.com>
2020-03-14 15:05:45 +0800

    🎂 Project initialized!

---
+++ src/data.rs
@@ -0,0 +1,151 @@
+use crate::decks::EMPTY_DECK;
+use std::{
+    fmt::{self, Display, Formatter},
+    ops::AddAssign,
+};
+
+#[derive(Default, Debug)]
+pub struct Data {
+    // 牌数
+    decks: u8,
+    samples: usize,
+    // 缺牌
+    has_miss: usize,
+    count_miss: usize,
+    // 单张
+    has_solo: usize,
+    count_solo: usize,
+    // 对子
+    has_pair: usize,
+    count_pair: usize,
+    // 飞机
+    has_trio: usize,
+    count_trio: usize,
+    // 炸弹
+    has_bomb: usize,
+    count_bomb: usize,
+    // 王炸
+    has_nuke: usize,
+}
+
+impl Data {
+    pub fn new(decks: u8, samples: usize) -> Data {
+        Data { decks, samples, ..Data::default() }
+    }
+    pub fn check(&mut self, cards: &[u8]) {
+        // println!("{:?}", cards);
+        let mut sorted = cards.to_vec();
+        sorted.sort_unstable();
+        let mut count = DataCount::default();
+        count.check_nuke(&mut sorted, self.decks);
+        let mut bin = EMPTY_DECK.clone();
+        for n in sorted {
+            bin[n as usize - 1] += 1
+        }
+        // println!("{:?}", bin)
+        for n in bin.iter() {
+            match n {
+                0 => count.count_miss += 1,
+                1 => count.count_solo += 1,
+                2 => count.count_pair += 1,
+                3 => count.count_trio += 1,
+                _ => count.count_bomb += 1,
+            }
+        }
+        self.add_assign(count);
+    }
+}
+
+#[derive(Default, Debug)]
+struct DataCount {
+    count_miss: isize,
+    count_solo: usize,
+    count_pair: usize,
+    count_trio: usize,
+    count_bomb: usize,
+    count_nuke: usize,
+}
+
+impl DataCount {
+    fn check_nuke(&mut self, sorted: &mut Vec<u8>, decks: u8) {
+        let len = sorted.len();
+        match decks {
+            1 => {
+                let end = sorted[len - 2..len].iter().map(|u| *u as usize).sum::<usize>();
+                if end == 29 {
+                    self.count_nuke += 1;
+                    sorted.pop();
+                    sorted.pop();
+                }
+            }
+            2 => {
+                let end = sorted[len - 4..len].iter().map(|u| *u as usize).sum::<usize>();
+                if end == 58 {
+                    self.count_nuke += 1;
+                    sorted.pop();
+                    sorted.pop();
+                    sorted.pop();
+                    sorted.pop();
+                }
+            }
+            3 => (),
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl AddAssign<DataCount> for Data {
+    fn add_assign(&mut self, rhs: DataCount) {
+        if rhs.count_nuke == 0 {
+            if rhs.count_miss != 0 {
+                self.has_miss += 1;
+                self.count_miss += rhs.count_miss as usize
+            }
+        }
+        // 如果有王炸, 缺牌数 -2
+        else {
+            self.has_nuke += 1;
+            if rhs.count_miss - 2 != 0 {
+                self.has_miss += 1;
+                if rhs.count_miss - 2 > 0 {
+                    self.count_miss += (rhs.count_miss - 2) as usize
+                }
+            }
+        }
+        if rhs.count_solo != 0 {
+            self.has_solo += 1;
+            self.count_solo += rhs.count_solo
+        }
+        if rhs.count_pair != 0 {
+            self.has_pair += 1;
+            self.count_pair += rhs.count_pair
+        }
+        if rhs.count_trio != 0 {
+            self.has_trio += 1;
+            self.count_trio += rhs.count_trio
+        }
+        if rhs.count_bomb != 0 {
+            self.has_bomb += 1;
+            self.count_bomb += rhs.count_bomb
+        }
+    }
+}
+
+impl Display for Data {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        writeln!(f, "- 总实验数: {}\n", self.samples)?;
+        writeln!(f, "缺牌概率: {:.4} %", self.has_miss as f64 / self.samples as f64 * 100.0)?;
+        writeln!(f, "缺牌期望: {:.4}", self.count_miss as f64 / self.samples as f64)?;
+        writeln!(f, "单张概率: {:.4} %", self.has_solo as f64 / self.samples as f64 * 100.0)?;
+        writeln!(f, "单张期望: {:.4}", self.count_solo as f64 / self.samples as f64)?;
+        writeln!(f, "对子概率: {:.4} %", self.has_pair as f64 / self.samples as f64 * 100.0)?;
+        writeln!(f, "对子期望: {:.4}", self.count_pair as f64 / self.samples as f64)?;
+        writeln!(f, "飞机概率: {:.4} %", self.has_trio as f64 / self.samples as f64 * 100.0)?;
+        writeln!(f, "飞机期望: {:.4}", self.count_trio as f64 / self.samples as f64)?;
+        writeln!(f, "炸弹概率: {:.4} %", self.has_bomb as f64 / self.samples as f64 * 100.0)?;
+        writeln!(f, "炸弹期望: {:.4}", self.count_bomb as f64 / self.samples as f64)?;
+        writeln!(f, "王炸概率: {:.4} %", self.has_nuke as f64 / self.samples as f64 * 100.0)?;
+        writeln!(f, "王炸期望: {:.4}", self.has_nuke as f64 / self.samples as f64)?;
+        Ok(())
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,134 @@
+mod data;
+#[allow(dead_code)]
+mod decks;
+
+use crate::{
+    data::Data,
+    decks::{ONE_DECK, TWO_DECK},
+};
+use rand::seq::SliceRandom;
+use std::fmt::Write as FmtWrite;
+use time::Instant;
+use std::fs::File;
+use std::io::Write;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let start = Instant::now();
+    let mut readme = String::new();
+    if let Ok(o) = one_deck() {
+        writeln!(readme, "{}", o)?
+    }
+    if let Ok(o) = two_deck() {
+        writeln!(readme, "{}", o)?
+    }
+    let mut file = File::create("readme.md")?;
+    file.write_all(readme.as_bytes())?;
+    let end = Instant::now();
+    println!("finished in {} s", (start - end).as_seconds_f32());
+    Ok(())
+}
+
+fn one_deck() -> Result<String, Box<dyn std::error::Error>> {
+    const SAMPLES: usize = 400_0000;
+    let mut cards17 = Data::new(1, SAMPLES * 2);
+    let mut cards18 = Data::new(1, SAMPLES * 3);
+    let mut cards20 = Data::new(1, SAMPLES);
+    for _ in 1..=SAMPLES {
+        let mut rng = rand::thread_rng();
+        let mut cards = ONE_DECK.to_vec();
+        cards.shuffle(&mut rng);
+        cards17.check(&cards[0..17]);
+        cards17.check(&cards[17..34]);
+        cards20.check(&cards[34..54]);
+    }
+    for _ in 1..=SAMPLES {
+        let mut rng = rand::thread_rng();
+        let mut cards = ONE_DECK.to_vec();
+        cards.shuffle(&mut rng);
+        cards18.check(&cards[0..18]);
+        cards18.check(&cards[18..36]);
+        cards18.check(&cards[36..54]);
+    }
+    let mut out = String::new();
+    writeln!(out, "###  三人斗地主")?;
+    writeln!(out, "- 地主: 20 张")?;
+    writeln!(out, "{}", cards20)?;
+    writeln!(out, "- 农民: 17 张")?;
+    writeln!(out, "{}", cards17)?;
+    writeln!(out, "###  三人扑克")?;
+    writeln!(out, "- 均分: 18 张")?;
+    writeln!(out, "{}", cards18)?;
+    return Ok(out);
+}
+
+fn two_deck() -> Result<String, Box<dyn std::error::Error>> {
+    const SAMPLES: usize = 200_0000;
+    let mut cards25 = Data::new(1, SAMPLES * 3);
+    let mut cards27 = Data::new(1, SAMPLES * 4);
+    let mut cards33 = Data::new(1, SAMPLES);
+    for _ in 1..=SAMPLES {
+        let mut rng = rand::thread_rng();
+        let mut cards = TWO_DECK.to_vec();
+        cards.shuffle(&mut rng);
+        cards25.check(&cards[0..25]);
+        cards25.check(&cards[25..50]);
+        cards25.check(&cards[50..75]);
+        cards33.check(&cards[75..108]);
+    }
+    for _ in 1..=SAMPLES {
+        let mut rng = rand::thread_rng();
+        let mut cards = TWO_DECK.to_vec();
+        cards.shuffle(&mut rng);
+        cards27.check(&cards[0..27]);
+        cards27.check(&cards[27..54]);
+        cards27.check(&cards[54..81]);
+        cards27.check(&cards[81..108]);
+    }
+    let mut out = String::new();
+    writeln!(out, "###  四人斗地主")?;
+    writeln!(out, "- 地主: 33 张")?;
+    writeln!(out, "{}", cards33)?;
+    writeln!(out, "- 农民: 25 张")?;
+    writeln!(out, "{}", cards25)?;
+    writeln!(out, "###  四人扑克")?;
+    writeln!(out, "- 均分: 27 张")?;
+    writeln!(out, "{}", cards27)?;
+    return Ok(out);
+}
+
+/*
+fn three_deck() -> Result<String, Box<dyn std::error::Error>> {
+    const SAMPLES: usize = 100_0000;
+    let mut cards17 = Data::new(1, SAMPLES * 4);
+    let mut cards18 = Data::new(1, SAMPLES * 5);
+    let mut cards20 = Data::new(1, SAMPLES);
+    for _ in 1..=SAMPLES {
+        let mut rng = rand::thread_rng();
+        let mut cards = THREE_DECK.to_vec();
+        cards.shuffle(&mut rng);
+        cards17.check(&cards[0..17]);
+        cards17.check(&cards[17..34]);
+        cards17.check(&cards[17..34]);
+        cards17.check(&cards[17..34]);
+        cards20.check(&cards[34..54]);
+    }
+    for _ in 1..=SAMPLES {
+        let mut rng = rand::thread_rng();
+        let mut cards = THREE_DECK.to_vec();
+        cards.shuffle(&mut rng);
+        cards18.check(&cards[0..18]);
+        cards18.check(&cards[18..36]);
+        cards18.check(&cards[36..54]);
+        cards18.check(&cards[36..54]);
+        cards18.check(&cards[36..54]);
+    }
+    let mut out = String::new();
+    writeln!(out, "地主: 20 张")?;
+    writeln!(out, "{}", cards20)?;
+    writeln!(out, "农民: 17 张")?;
+    writeln!(out, "{}", cards17)?;
+    writeln!(out, "均分: 18 张")?;
+    writeln!(out, "{}", cards17)?;
+    return Ok(out);
+}
+*/
